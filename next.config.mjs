48bb4f888a50adf9fb4888ce6f415ea17a05fa44
Mazen Embaby <eng.mazen.embaby@gmail.com>
2026-08-20 04:35:29 +0400

    feat: source Map expirment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  experimental: {
+    serverSourceMaps: true,
+  },
   serverExternalPackages: ['firebase-admin', 'jwks-rsa', 'jose', 'ws', '@neondatabase/serverless'],
 }
 
